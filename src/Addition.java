85bc09a065eea2d2e2802713662f9d818877a97b
NeeruParmar <neeruparmarca@gmail.com>
2021-12-18 16:46:18 -0500

    Addition

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Addidtion of two numbers
+				int numa = 8;
+				int numb = 8;
+				int Sum = numa + numb;
+				System.out.println(Sum);
+		
+	}
+
+}
