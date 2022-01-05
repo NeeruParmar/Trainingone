f28f16f732606023f3ee5907d20417d4ccafdea7
NeeruParmar <neeruparmarca@gmail.com>
2022-01-05 00:12:56 -0500

    ifelsequestions

---
+++ src/AssigJan.java
@@ -0,0 +1,73 @@
+
+public class AssigJan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Question 1
+		 int numa=89;
+		 if (numa>=0 & numa<50) {
+			 System.out.println("Grade D"); 
+		 } else if  (numa>=50 & numa<60) {
+			 System.out.println("Garde C");
+		 } else if (numa>=60 & numa<75) {
+			 System.out.println("Grade B");
+		 }else {
+			 System.out.println("Grade A");}
+		 
+		 
+		 
+		 //question 3: greatest of three numbers 
+		 
+		 int num1=66,num2=99, num3=67;
+		 if (num1>num2 && num1>num3) {
+			 System.out.println("num1 is the greatest");
+		 }else if (num2>num1 && num2 >num3) {
+			 System.out.println("num2 is the greatest");
+		 }else {
+			 System.out.println("num3 is the greatest");
+		 }
+		 
+		 //Ques4 Write an application that reads two integers, determines whether the first is a multiple of
+		 // the second and prints the result.
+		 int numx= 85;
+		 int numy= 17;
+		 if (numx%numy==0) {
+			 System.out.println("First number is multiple of second");
+		 }else {
+			 System.out.println("not a multiple");
+			 
+		 }
+		 //Quest 7
+		 int h1=99;
+		 if (h1>=200) {
+			 System.out.println("Not Eligible for Ticket");
+		 }else if ( h1<90) {
+			 System.out.println("Not Eligible for Ticket");
+			  }else {
+			 System.out.println("Eligible for Ticket");
+		 }
+		
+			 
+		 }
+		 
+		 
+		 
+		 
+		
+		 
+		 
+		 
+
+		 
+			 
+	
+		
+}
+	
+	
+
+			
+	
+	
+
+
