ead572e6fe0bff563645d61cab207fee3beb0ce2
NeeruParmar <neeruparmarca@gmail.com>
2022-02-11 17:02:58 -0500

    correction done

--- AdditionalClass/MethodsQuestion/Main.java
+++ AdditionalClass/MethodsQuestion/Main.java
@@ -6,10 +6,10 @@ public class Main {
 		// TODO Auto-generated method stub
 //ques 1: Write a method named isEven that accepts an int argument. 
 // The method should return true if the argument is even, or false otherwise. Call this method from main class and print the results.		
-		
-        EvenNum obj = new EvenNum ();
-        boolean IsNumEven =obj.isEven(8);
-        System.out.println("Given number is even "+IsNumEven );
+
+		EvenNum obj = new EvenNum();
+		boolean isNumEven = obj.isEven(8);
+		System.out.println("Given number is even " + isNumEven);
 
 	}
 
